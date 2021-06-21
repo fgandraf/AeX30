a3e0e4aa9bab8cc4c2a8657a475f3c55bc99e82a
Felipe Gandra <Felipe Gandra@DESKTOP-0AKMI97>
2021-06-21 19:50:11 -0300

    Refatoracao: Populate from PFUI

--- FormMain.cs
+++ FormMain.cs
@@ -4,7 +4,6 @@ using System.Windows.Forms;
 using ClosedXML.Excel;
 using System.Runtime.InteropServices;
 using System.IO;
-using System.Collections.Generic;
 
 namespace Raecef
 {
@@ -14,6 +13,25 @@ namespace Raecef
         private extern static void ReleaseCapture();
         [DllImport("user32.DLL", EntryPoint = "SendMessage")]
         private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+
+
+        //Linha 1: Cabeçalho | Linha 2: Orçamento | Linha 3: Cronograma-1 | Linha 4: Cronograma-2 
+        string[] ae130v018 = new string[]
+        {
+            "G42", "AA42", "AG42", "AI42", "AN42", "AX42", "BD42", "BF42", "BL42", "BN42", "G46", "AP46", "BA46", "BF46", "G48", "AB48", "AG50", "AP50", "AX50", "BF50", "BN50",
+            "AR116", "AR118", "AR130", "AR137", "AR146", "AR156", "AR165", "AR172", "AR179", "AR190", "AR197", "AR207", "AR217", "AR228", "AR234", "AR245", "AR254", "AR262", "AR271", "AR273",
+            "AJ311", "AL310", "AP310", "AT310", "AX310", "BB310", "BF310", "BJ310", "BN310",
+            "AL350", "AP350", "AT350", "AX350", "BB350", "BF350", "BJ350", "BN350"
+        };
+
+        string[] ae130v020 = new string[]
+        {
+            "G42", "AA42", "AG42", "AI42", "AN42", "AX42", "BD42", "BF42", "BL42", "BN42", "G46", "AP46", "BA46", "BF46", "G48", "AB48", "AG50", "AP50", "AX50", "BF50", "BN50",
+            "AR116", "AR118", "AR130", "AR137", "AR146", "AR156", "AR165", "AR172", "AR179", "AR190", "AR197", "AR207", "AR217", "AR228", "AR234", "AR245", "AR254", "AR262", "AR271", "AR273",
+            "AJ312", "AL311", "AP311", "AT311", "AX311", "BB311", "BF311", "BJ311", "BN311",
+            "AL351", "AP351", "AT351", "AX351", "BB351", "BF351", "BJ351", "BN351"
+        };
 
 
 
@@ -73,7 +91,7 @@ namespace Raecef
                            .ToList();
 
                     string referencia = Util.CleanInput(convocacao[0]);
-                    
+
                     if (referencia.Substring(0, 1) == "0")
                         referencia = referencia.Substring(1);
 
@@ -105,107 +123,97 @@ namespace Raecef
                 {
                     var xls = new XLWorkbook(openExcel.FileName);
                     var planilha = xls.Worksheets.First(w => w.Name == "Proposta");
+                    string[] aeX;
 
-                    //CABEÇALHO
+
+                    //CHECK VERSION & CREATE RELATED ARRAY
                     {
-                        txtPropNome.Text = planilha.Cell("G42").RichText.ToString().ToUpper();
-                        txtPropCPF.Text = Util.FormatedCPF(planilha.Cell("AA42").RichText.ToString());
-                        txtPropDDD.Text = planilha.Cell("AG42").RichText.ToString();
-                        txtPropTelefone.Text = Util.FormatedFone(planilha.Cell("AI42").RichText.ToString());
-                        txtRTNome.Text = planilha.Cell("AN42").RichText.ToString().ToUpper();
-                        txtRTCauCrea.Text = planilha.Cell("AX42").RichText.ToString().Replace(',', '.');
-                        txtRTUF.Text = planilha.Cell("BD42").RichText.ToString();
-                        txtRTCPF.Text = Util.FormatedCPF(planilha.Cell("BF42").Value.ToString());
-                        txtRTDDD.Text = planilha.Cell("BL42").RichText.ToString();
-                        txtRTTelefone.Text = Util.FormatedFone(planilha.Cell("BN42").RichText.ToString());
-                        txtIdEndereco.Text = planilha.Cell("G46").RichText.ToString().ToUpper();
-                        txtIdComplemento.Text = planilha.Cell("AP46").RichText.ToString().ToUpper();
-                        txtIdCEP.Text = Util.FormatedCEP(planilha.Cell("BA46").RichText.ToString());
-                        txtIdBairro.Text = planilha.Cell("BF46").RichText.ToString().ToUpper();
-                        txtIdMunicipio.Text = planilha.Cell("G48").RichText.ToString().ToUpper();
-                        txtIdUF.Text = planilha.Cell("AB48").RichText.ToString();
-                        txtTerrenoValorProposto.Text = string.Format("{0:0,0.00}", Convert.ToDouble(planilha.Cell("AG50").CachedValue));
-                        txtTerrenoMatricula.Text = planilha.Cell("AP50").RichText.ToString().Replace(',', '.');
-                        txtTerrenoOficio.Text = planilha.Cell("AX50").RichText.ToString().ToUpper();
-                        txtTerrenoComarca.Text = planilha.Cell("BF50").RichText.ToString().ToUpper();
-                        txtTerrenoUF.Text = planilha.Cell("BN50").RichText.ToString();
-                    }
-
-                    //ORÇAMENTO (PERCENTUAIS)
-                    {
-                        txt1701.Text = planilha.Cell("AR116").CachedValue.ToString().Replace('.', ',');
-                        txt1702.Text = planilha.Cell("AR118").CachedValue.ToString().Replace('.', ',');
-                        txt1703.Text = planilha.Cell("AR130").CachedValue.ToString().Replace('.', ',');
-                        txt1704.Text = planilha.Cell("AR137").CachedValue.ToString().Replace('.', ',');
-                        txt1705.Text = planilha.Cell("AR146").CachedValue.ToString().Replace('.', ',');
-                        txt1706.Text = planilha.Cell("AR156").CachedValue.ToString().Replace('.', ',');
-                        txt1707.Text = planilha.Cell("AR165").CachedValue.ToString().Replace('.', ',');
-                        txt1708.Text = planilha.Cell("AR172").CachedValue.ToString().Replace('.', ',');
-                        txt1709.Text = planilha.Cell("AR179").CachedValue.ToString().Replace('.', ',');
-                        txt1710.Text = planilha.Cell("AR190").CachedValue.ToString().Replace('.', ',');
-                        txt1711.Text = planilha.Cell("AR197").CachedValue.ToString().Replace('.', ',');
-                        txt1712.Text = planilha.Cell("AR207").CachedValue.ToString().Replace('.', ',');
-                        txt1713.Text = planilha.Cell("AR217").CachedValue.ToString().Replace('.', ',');
-                        txt1714.Text = planilha.Cell("AR228").CachedValue.ToString().Replace('.', ',');
-                        txt1715.Text = planilha.Cell("AR234").CachedValue.ToString().Replace('.', ',');
-                        txt1716.Text = planilha.Cell("AR245").CachedValue.ToString().Replace('.', ',');
-                        txt1717.Text = planilha.Cell("AR254").CachedValue.ToString().Replace('.', ',');
-                        txt1718.Text = planilha.Cell("AR262").CachedValue.ToString().Replace('.', ',');
-                        txt1719.Text = planilha.Cell("AR271").CachedValue.ToString().Replace('.', ',');
-                        txt1720.Text = planilha.Cell("AR273").CachedValue.ToString().Replace('.', ',');
-                    }
-
-                    //CRONOGRAMA - PRIMEIRA PÁGINA
-                    {
-                        string linha18_50_01;
-
                         if (planilha.Cell("G310").CachedValue.ToString() == "18.50.01")
-                            linha18_50_01 = "310";
+                            aeX = ae130v018;
                         else if (planilha.Cell("G311").CachedValue.ToString() == "18.50.01")
-                            linha18_50_01 = "311";
+                            aeX = ae130v020;
                         else
-                            linha18_50_01 = "";
-
-
-                        txtExecutado.Text = planilha.Cell("AJ" + (Convert.ToInt32(linha18_50_01) + 1).ToString()).CachedValue.ToString().Replace('.', ',');
-                        txtParcela1.Text = planilha.Cell("AL" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela2.Text = planilha.Cell("AP" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela3.Text = planilha.Cell("AT" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela4.Text = planilha.Cell("AX" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela5.Text = planilha.Cell("BB" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela6.Text = planilha.Cell("BF" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela7.Text = planilha.Cell("BJ" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
-                        txtParcela8.Text = planilha.Cell("BN" + linha18_50_01).CachedValue.ToString().Replace('.', ',');
+                            aeX = null;
                     }
 
-                    //CRONOGRAMA - SEGUNDA PÁGINA
+
+                    //POPULATE TEXTBOX
                     {
-                        string linha18_50_02;
-
-                        if (planilha.Cell("G350").CachedValue.ToString() == "18.50.02")
-                            linha18_50_02 = "350";
-                        else if (planilha.Cell("G351").CachedValue.ToString() == "18.50.02")
-                            linha18_50_02 = "351";
-                        else
-                            linha18_50_02 = "";
-
-                        txtParcela9.Text = planilha.Cell("AL" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela10.Text = planilha.Cell("AP" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela11.Text = planilha.Cell("AT" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela12.Text = planilha.Cell("AX" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela13.Text = planilha.Cell("BB" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela14.Text = planilha.Cell("BF" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela15.Text = planilha.Cell("BJ" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
-                        txtParcela16.Text = planilha.Cell("BN" + linha18_50_02).CachedValue.ToString().Replace('.', ',');
+                        //CABEÇALHO
+                        txtPropNome.Text = planilha.Cell(aeX[0]).RichText.ToString().ToUpper();
+                        txtPropCPF.Text = Util.FormatedCPF(planilha.Cell(aeX[1]).RichText.ToString());
+                        txtPropDDD.Text = planilha.Cell(aeX[2]).RichText.ToString();
+                        txtPropTelefone.Text = Util.FormatedFone(planilha.Cell(aeX[3]).RichText.ToString());
+                        txtRTNome.Text = planilha.Cell(aeX[4]).RichText.ToString().ToUpper();
+                        txtRTCauCrea.Text = planilha.Cell(aeX[5]).RichText.ToString().Replace(',', '.');
+                        txtRTUF.Text = planilha.Cell(aeX[6]).RichText.ToString();
+                        txtRTCPF.Text = Util.FormatedCPF(planilha.Cell(aeX[7]).Value.ToString());
+                        txtRTDDD.Text = planilha.Cell(aeX[8]).RichText.ToString();
+                        txtRTTelefone.Text = Util.FormatedFone(planilha.Cell(aeX[9]).RichText.ToString());
+                        txtIdEndereco.Text = planilha.Cell(aeX[10]).RichText.ToString().ToUpper();
+                        txtIdComplemento.Text = planilha.Cell(aeX[11]).RichText.ToString().ToUpper();
+                        txtIdCEP.Text = Util.FormatedCEP(planilha.Cell(aeX[12]).RichText.ToString());
+                        txtIdBairro.Text = planilha.Cell(aeX[13]).RichText.ToString().ToUpper();
+                        txtIdMunicipio.Text = planilha.Cell(aeX[14]).RichText.ToString().ToUpper();
+                        txtIdUF.Text = planilha.Cell(aeX[15]).RichText.ToString();
+                        txtTerrenoValorProposto.Text = string.Format("{0:0,0.00}", Convert.ToDouble(planilha.Cell(aeX[16]).CachedValue));
+                        txtTerrenoMatricula.Text = planilha.Cell(aeX[17]).RichText.ToString().Replace(',', '.');
+                        txtTerrenoOficio.Text = planilha.Cell(aeX[18]).RichText.ToString().ToUpper();
+                        txtTerrenoComarca.Text = planilha.Cell(aeX[19]).RichText.ToString().ToUpper();
+                        txtTerrenoUF.Text = planilha.Cell(aeX[20]).RichText.ToString();
+                        //ORÇAMENTO (PERCENTUAIS)
+                        txt1701.Text = planilha.Cell(aeX[21]).CachedValue.ToString().Replace('.', ',');
+                        txt1702.Text = planilha.Cell(aeX[22]).CachedValue.ToString().Replace('.', ',');
+                        txt1703.Text = planilha.Cell(aeX[23]).CachedValue.ToString().Replace('.', ',');
+                        txt1704.Text = planilha.Cell(aeX[24]).CachedValue.ToString().Replace('.', ',');
+                        txt1705.Text = planilha.Cell(aeX[25]).CachedValue.ToString().Replace('.', ',');
+                        txt1706.Text = planilha.Cell(aeX[26]).CachedValue.ToString().Replace('.', ',');
+                        txt1707.Text = planilha.Cell(aeX[27]).CachedValue.ToString().Replace('.', ',');
+                        txt1708.Text = planilha.Cell(aeX[28]).CachedValue.ToString().Replace('.', ',');
+                        txt1709.Text = planilha.Cell(aeX[29]).CachedValue.ToString().Replace('.', ',');
+                        txt1710.Text = planilha.Cell(aeX[30]).CachedValue.ToString().Replace('.', ',');
+                        txt1711.Text = planilha.Cell(aeX[31]).CachedValue.ToString().Replace('.', ',');
+                        txt1712.Text = planilha.Cell(aeX[32]).CachedValue.ToString().Replace('.', ',');
+                        txt1713.Text = planilha.Cell(aeX[33]).CachedValue.ToString().Replace('.', ',');
+                        txt1714.Text = planilha.Cell(aeX[34]).CachedValue.ToString().Replace('.', ',');
+                        txt1715.Text = planilha.Cell(aeX[35]).CachedValue.ToString().Replace('.', ',');
+                        txt1716.Text = planilha.Cell(aeX[36]).CachedValue.ToString().Replace('.', ',');
+                        txt1717.Text = planilha.Cell(aeX[37]).CachedValue.ToString().Replace('.', ',');
+                        txt1718.Text = planilha.Cell(aeX[38]).CachedValue.ToString().Replace('.', ',');
+                        txt1719.Text = planilha.Cell(aeX[39]).CachedValue.ToString().Replace('.', ',');
+                        txt1720.Text = planilha.Cell(aeX[40]).CachedValue.ToString().Replace('.', ',');
+                        //CRONOGRAMA - PRIMEIRA PÁGINA
+                        txtExecutado.Text = planilha.Cell(aeX[41]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela1.Text = planilha.Cell(aeX[42]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela2.Text = planilha.Cell(aeX[43]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela3.Text = planilha.Cell(aeX[44]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela4.Text = planilha.Cell(aeX[45]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela5.Text = planilha.Cell(aeX[46]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela6.Text = planilha.Cell(aeX[47]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela7.Text = planilha.Cell(aeX[48]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela8.Text = planilha.Cell(aeX[49]).CachedValue.ToString().Replace('.', ',');
+                        //CRONOGRAMA - SEGUNDA PÁGINA
+                        txtParcela9.Text = planilha.Cell(aeX[50]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela10.Text = planilha.Cell(aeX[51]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela11.Text = planilha.Cell(aeX[52]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela12.Text = planilha.Cell(aeX[53]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela13.Text = planilha.Cell(aeX[54]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela14.Text = planilha.Cell(aeX[55]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela15.Text = planilha.Cell(aeX[56]).CachedValue.ToString().Replace('.', ',');
+                        txtParcela16.Text = planilha.Cell(aeX[57]).CachedValue.ToString().Replace('.', ',');
                     }
 
 
-                    //Type t = pfui.GetType();
-                    //PropertyInfo[] pi = t.GetProperties();
-                    //foreach (PropertyInfo p in pi)
-                    //{
-                    //    txtBox.Text += p.Name + ": " + p.GetValue(pfui) + "\r\n";
-                    //}
+                    //IGNORED
+                    {
+                        //Função exibe no textBox todos os parâmetros(valores) de um objeto
+                        //Type t = pfui.GetType();
+                        //PropertyInfo[] pi = t.GetProperties();
+                        //foreach (PropertyInfo p in pi)
+                        //{
+                        //    txtBox.Text += p.Name + ": " + p.GetValue(pfui) + "\r\n";
+                        //}
+                    }
 
                     pnlMainPfui.Show();
                     btnProximoPfui.Show();
